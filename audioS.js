dc7b85c95ca7dfea2557dfb842a7bbcbfe132fa2
tmuchicken <tmuchicken@yahoo.co.jp>
2018-10-19 14:25:31 +0900

    update

--- audioS.js
+++ audioS.js
@@ -138,7 +138,7 @@ function stopStream(stream) {
 
 
         var merger = context.createChannelMerger(2);
-        StereoPanner1.connect(merger,0,1);
+        StereoPanner1.connect(merger,0,0);
         StereoPanner2.connect(merger,1,0);
         
 
